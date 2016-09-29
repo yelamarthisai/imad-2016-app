512390bf9d33cb68efc65fccec4f20206e1efa5a
yelamarthisai <yelamarthisai@gmail.com>
2016-09-29 22:34:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  es.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
